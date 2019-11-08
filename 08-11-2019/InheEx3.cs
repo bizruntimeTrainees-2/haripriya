ba37aaf055abab5e250dedba228db71032cfc397
unknown <anapalaharipriya1998@gmail.com>
2019-11-08 18:51:29 +0530

    Programs

---
+++ 08-11-2019/InheEx3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+    class Tutorial
+    {
+        protected string TutorialName;
+            public String GetTutorial()
+            {
+            return TutorialName;
+            }
+    }
+ class InheEx3 : Tutorial
+    {
+        public void RenameTutorial(String pNewName)
+        {
+            TutorialName = pNewName;
+        }
+
+        static void Main(string[] args)
+        {
+            InheEx3 pTutor = new InheEx3();
+
+            pTutor.RenameTutorial(".Net by Guru99");
+
+            Console.WriteLine(pTutor.GetTutorial());
+
+            Console.ReadKey();
+        }
+    }
