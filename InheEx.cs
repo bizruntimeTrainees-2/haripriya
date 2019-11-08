e464a36186f978870a65ed48d825c02911ddfbd5
unknown <anapalaharipriya1998@gmail.com>
2019-11-08 18:31:42 +0530

    Programs

---
+++ InheEx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace ConsoleApplication1
+{
+    class GFG
+    {
+        public string name;
+        public string subject;
+        public void readers(string name, string subject)
+        {
+            this.name = name;
+            this.subject = subject;
+            Console.WriteLine("Myself: " + name);
+            Console.WriteLine("My Favorite Subject is: " + subject);
+        }
+    }
+    class GeeksforGeeks : GFG
+    {
+        public GeeksforGeeks()
+        {
+            Console.WriteLine("GeeksforGeeks");
+        }
+    }
+    class Sudo
+    {
+        static void Main(string[] args)
+        {
+            GeeksforGeeks g = new GeeksforGeeks();
+            g.readers("Kirti", "C#");
+        }
+    }
+}
