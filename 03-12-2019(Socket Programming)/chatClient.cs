dd94295250f1c84f97d07cf28870f7e3cc120107
haripriya@504 <haripriya@504>
2019-12-03 09:14:51 +0530

    Programs

---
+++ 03-12-2019(Socket Programming)/chatClient.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
+using SocketProgramming;
+
+namespace WindowsApplication2
+{
+    public partial class chat
+    {
+        System.Net.Sockets.TcpClient clientSocket = new System.Net.Sockets.TcpClient();
+        NetworkStream serverStream = default(NetworkStream);
+        string readData = null;
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte[] outStream = System.Text.Encoding.ASCII.GetBytes("$");
+            serverStream.Write(outStream, 0, outStream.Length);
+            serverStream.Flush();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            readData = "Conected to Chat Server ...";
+            //Chat Server and Chat Client are running on the same machine 
+            clientSocket.Connect("127.0.0.1",8080);
+            serverStream = clientSocket.GetStream();
+
+            byte[] outStream = System.Text.Encoding.ASCII.GetBytes("$");
+            serverStream.Write(outStream, 0, outStream.Length);
+            serverStream.Flush();
+
+            Thread ctThread = new Thread(getMessage);
+            ctThread.Start();
+        }
+
+        private void getMessage()
+        {
+            while (true)
+            {
+                serverStream = clientSocket.GetStream();
+                int buffSize = 0;
+                byte[] inStream = new byte[10025];
+                buffSize = clientSocket.ReceiveBufferSize;
+                serverStream.Read(inStream, 0, buffSize);
+                string returndata = System.Text.Encoding.ASCII.GetString(inStream);
+                readData = "" + returndata;
+               
+            }
+        }
+
+    }
+}
---
+++ 03-12-2019(Socket Programming)/chatServer.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net.Sockets;
+using System.Collections;
+
+namespace SocketProgramming
+{
+    //Chat Server here is to listen to the incoming request from Clients, and when the Server got a message, 
+   //it Broadcast the message to all the Clients currently connected to the Chat Server
+    class chatServer
+    {
+        //client list we implemented as a hash table.
+        public static Hashtable clientsList = new Hashtable();
+
+        static void Main(string[] args)
+        {
+            TcpListener serverSocket = new TcpListener(8888);
+            TcpClient clientSocket = default(TcpClient);
+            int counter = 0;
+
+            serverSocket.Start();
+            Console.WriteLine("Chat Server Started ....");
+            counter = 0;
+            while ((true))
+            {
+                counter += 1;
+                clientSocket = serverSocket.AcceptTcpClient();
+
+                byte[] bytesFrom = new byte[10025];
+                string dataFromClient = null;
+
+                NetworkStream networkStream = clientSocket.GetStream();
+                networkStream.Read(bytesFrom, 0, (int)clientSocket.ReceiveBufferSize);
+                dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);
+                dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
+
+                //when the server got a connewction request,it add name of the client into clientList
+                //create a new thread for communicate with server
+                clientsList.Add(dataFromClient, clientSocket);
+
+
+                //when the server gets a msg from any client,it select all the client
+                //from the clientsList and send the message to all client in clientsList
+                broadcast(dataFromClient + " Joined ", dataFromClient, false);
+
+                Console.WriteLine(dataFromClient + " Joined chat room ");
+
+                //when a client conected to server,the server create a new thread for
+                //communication.we implement a cls handleclient for handling client as a seperate thread
+                handleClinet client = new handleClinet();
+                client.startClient(clientSocket, dataFromClient, clientsList);
+            }
+
+            clientSocket.Close();
+            serverSocket.Stop();
+            Console.WriteLine("exit");
+            Console.ReadLine();
+        }
+
+        public static void broadcast(string msg, string uName, bool flag)
+        {
+            foreach (DictionaryEntry Item in clientsList)
+            {
+                TcpClient broadcastSocket;
+                broadcastSocket = (TcpClient)Item.Value;
+                NetworkStream broadcastStream = broadcastSocket.GetStream();
+                Byte[] broadcastBytes = null;
+
+                if (flag == true)
+                {
+                    broadcastBytes = Encoding.ASCII.GetBytes(uName + " says : " + msg);
+                }
+                else
+                {
+                    broadcastBytes = Encoding.ASCII.GetBytes(msg);
+                }
+
+                broadcastStream.Write(broadcastBytes, 0, broadcastBytes.Length);
+                broadcastStream.Flush();
+            }
+        }  //end broadcast function
+    }//end Main class
+
+
+    public class handleClinet
+    {
+        TcpClient clientSocket;
+        string clNo;
+        Hashtable clientsList;
+
+        public void startClient(TcpClient inClientSocket, string clineNo, Hashtable cList)
+        {
+            this.clientSocket = inClientSocket;
+            this.clNo = clineNo;
+            this.clientsList = cList;
+            Thread ctThread = new Thread(doChat);
+            ctThread.Start();
+        }
+
+        //handling the commu b/w the serverside client
+        private void doChat()
+        {
+            int requestCount = 0;
+            byte[] bytesFrom = new byte[10025];
+            string dataFromClient = null;
+            Byte[] sendBytes = null;
+            string serverResponse = null;
+            string rCount = null;
+            requestCount = 0;
+
+            while ((true))
+            {
+                try
+                {
+                    requestCount = requestCount + 1;
+                    NetworkStream networkStream = clientSocket.GetStream();
+                    networkStream.Read(bytesFrom, 0, (int)clientSocket.ReceiveBufferSize);
+                    dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);
+                    dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
+                    Console.WriteLine("From client - " + clNo + " : " + dataFromClient);
+                    rCount = Convert.ToString(requestCount);
+
+                    chatServer.broadcast(dataFromClient, clNo, true);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+        }
+    } 
+}
+//when server gets a msgs from any of the connected chat client,the server
+//broadcast the msg to all client
