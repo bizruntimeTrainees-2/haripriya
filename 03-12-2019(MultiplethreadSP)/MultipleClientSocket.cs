178972572a77860eea3dea088ba795ac693cb43d
haripriya@504 <haripriya@504>
2019-12-03 20:19:10 +0530

    Programs

---
+++ 03-12-2019(MultiplethreadSP)/MultipleClientSocket.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+
+public class EchoClient
+{
+    public static void Main()
+    {
+        try
+        {
+           TcpClient client= new TcpClient("127.0.0.1", 8080);
+            StreamReader reader = new StreamReader(client.GetStream());
+            StreamWriter writer = new StreamWriter(client.GetStream());
+            string s = String.Empty;
+            while (!s.Equals("Exit"))
+            {
+                Console.WriteLine("Enter a string to send server:");
+                s = Console.ReadLine();
+                Console.WriteLine();
+                writer.WriteLine(s);
+                writer.Flush();
+                String server_string = reader.ReadLine();
+                Console.WriteLine(server_string);
+            }
+            reader.Close();
+            writer.Close();
+            client.Close();
+        }catch(Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+
+}
---
+++ 03-12-2019(MultiplethreadSP)/MultipleServerSocket.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+public class MultipleThreadEchoServer
+{
+    private static void ProcessClientRequest(object argument)
+    {
+        TcpClient client = (TcpClient)argument;
+        try
+        {
+            StreamReader reader = new StreamReader(client.GetStream());
+            StreamWriter writer = new StreamWriter(client.GetStream());
+            string s = String.Empty;
+            while (!(s = reader.ReadLine()).Equals("Exit") || (s == null))
+            {
+                Console.WriteLine("From Client ->" + s);
+                writer.WriteLine("From Client ->" + s);
+                writer.Flush();
+            }
+            reader.Close();
+            writer.Close();
+            client.Close();
+            Console.WriteLine("Closing Client Connection:");
+        }
+        catch (IOException)
+        {
+            Console.WriteLine("problem with Client connection,existing thread");
+        }
+        finally
+        {
+            if (client != null)
+            {
+                client.Close();
+            }
+        }
+    }
+    static void Main()
+    {
+        TcpListener listener = null;
+        try
+        {
+            listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 8080);
+            listener.Start();
+            Console.WriteLine("Multiple ThreadEcho server started...");
+            while (true)
+            {
+                Console.WriteLine("waiting for incoming client connection....");
+                TcpClient client = listener.AcceptTcpClient();
+                Console.WriteLine("Accepted new client connection");
+                Thread t = new Thread(ProcessClientRequest);
+                t.Start(client);
+            }
+        } catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+        finally
+        {
+            if (listener != null)
+            {
+                listener.Stop();
+            }
+        }
+    }
+}
