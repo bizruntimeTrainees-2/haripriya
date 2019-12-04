5da696c2927de8d268f26837cff1a9f960c49389
haripriya@504 <haripriya@504>
2019-12-04 20:11:22 +0530

    proframs

---
+++ 04-12-2019(SocketProgramming)/ClientSocket1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Net.Sockets;
+using System.IO;
+public class ClientSocket1
+{
+    static void Main(string[] args)
+    {
+        TcpClient socketForServer;
+        bool status = true;
+        try
+        {
+            socketForServer = new TcpClient("localhost", 8100);
+            Console.WriteLine("Connected to Server");
+        }
+        catch
+        {
+            Console.WriteLine("Failed to Connect to server{0}:999", "localhost");
+            return;
+        }
+        NetworkStream networkStream = socketForServer.GetStream();
+        StreamReader streamreader = new StreamReader(networkStream);
+        StreamWriter streamwriter = new StreamWriter(networkStream);
+        try
+        {
+            string clientmessage = "";
+            string servermessage = "";
+            while (status)
+            {
+                Console.Write("Client:");
+                clientmessage = Console.ReadLine();
+                if ((clientmessage == "bye") || (clientmessage == "BYE"))
+                {
+                    status = false;
+                    streamwriter.WriteLine("bye");
+                    streamwriter.Flush();
+                }
+                if ((clientmessage != "bye") && (clientmessage != "BYE"))
+                {
+                    streamwriter.WriteLine(clientmessage);
+                    streamwriter.Flush();
+                    servermessage = streamreader.ReadLine();
+                    Console.WriteLine("Server:" + servermessage);
+                }
+            }
+        }
+        catch
+        {
+            Console.WriteLine("Exception reading from the server");
+        }
+        streamreader.Close();
+        networkStream.Close();
+        streamwriter.Close();
+    }
+}
+/*
+ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SimpleTcpClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            byte[] data = new byte[1024];
+
+            string input, stringData;
+
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"),9050);
+
+            Socket server = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+
+            try
+            {
+                server.Connect(ipep);
+
+            }
+            catch(SocketException e)
+            {
+                Console.WriteLine("Unable to connect to server.");
+
+                Console.WriteLine(e.ToString());
+
+                return;
+            }
+
+            int recv = server.Receive(data);
+
+            stringData = Encoding.UTF8.GetString(data, 0, recv);
+
+            Console.WriteLine(stringData);
+
+            while (true)
+            {
+                input = Console.ReadLine();
+
+                if (input == "exit")
+
+                    break;
+
+                Console.WriteLine("You: " + input);
+
+                server.Send(Encoding.UTF8.GetBytes(input));
+
+                data = new byte[1024];
+
+                recv = server.Receive(data);
+
+                stringData = Encoding.UTF8.GetString(data, 0, recv);
+
+                byte[] utf8string = System.Text.Encoding.UTF8.GetBytes(stringData);
+
+                Console.WriteLine("Server: "+stringData);
+            }
+
+            Console.WriteLine("Disconnecting from server...");
+
+            server.Shutdown(SocketShutdown.Both);
+
+            server.Close();
+
+            Console.WriteLine("Disconnected!");
+
+            Console.ReadLine();
+
+        }
+    }
+}
+*/ 
---
+++ 04-12-2019(SocketProgramming)/ServerSocket1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Net.Sockets;
+using System.IO;
+public class ServerSocket1
+{
+    public static void Main()
+    {
+        try
+        {
+            bool status = true;
+            string servermessage = "";
+            string clientmessage = "";
+            TcpListener tcpListener = new TcpListener(8100);
+            tcpListener.Start();
+            Console.WriteLine("Server Started");
+            Socket socketForClient = tcpListener.AcceptSocket();
+            Console.WriteLine("Client Connected");
+            NetworkStream networkStream = new NetworkStream(socketForClient);
+            StreamWriter streamwriter = new StreamWriter(networkStream);
+            StreamReader streamreader = new StreamReader(networkStream);
+            while (status)
+            {
+                if (socketForClient.Connected)
+                {
+                    servermessage = streamreader.ReadLine();
+                    Console.WriteLine("Client:" + servermessage);
+                    if ((servermessage == "bye"))
+                    {
+                        status = false;
+                        streamreader.Close();
+                        networkStream.Close();
+                        streamwriter.Close();
+                        return;
+                    }
+                    Console.Write("Server:");
+                    clientmessage = Console.ReadLine();
+                    streamwriter.WriteLine(clientmessage);
+                    streamwriter.Flush();
+                }
+            }
+            streamreader.Close();
+            networkStream.Close();
+            streamwriter.Close();
+            socketForClient.Close();
+            Console.WriteLine("Exiting");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+}
+/*
+ * using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SimpleTcpSrvr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            int recv;
+
+            byte[] data = new byte[1024];
+
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"),9050);
+
+            Socket newsock = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+
+            newsock.Bind(ipep);
+
+            newsock.Listen(10);
+
+            Console.WriteLine("Waiting for a client...");
+
+            Socket client = newsock.Accept();
+
+            IPEndPoint clientep = (IPEndPoint)client.RemoteEndPoint;
+
+            Console.WriteLine("Connected with {0} at port {1}",clientep.Address,clientep.Port);
+
+            string welcome = "Welcome to my test server";
+
+            data = Encoding.UTF8.GetBytes(welcome);
+
+            client.Send(data,data.Length,SocketFlags.None);
+
+            string input;
+
+            while (true)
+            {
+
+                data = new byte[1024];
+
+                recv = client.Receive(data);
+
+                if (recv == 0)
+
+                break;
+
+                Console.WriteLine("Client: "+Encoding.UTF8.GetString(data, 0, recv));
+
+                input = Console.ReadLine();
+
+                Console.WriteLine("You: " + input);
+
+                client.Send(Encoding.UTF8.GetBytes(input));
+            }
+
+            Console.WriteLine("Disconnected from {0}", clientep.Address);
+
+            client.Close();
+
+            newsock.Close();
+
+            Console.ReadLine();
+
+        }
+    }
+}
+*/
