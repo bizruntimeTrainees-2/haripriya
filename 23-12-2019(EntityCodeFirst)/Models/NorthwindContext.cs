a1b40311bd86e569902b63927454e5bbe2241060
haripriya@504 <haripriya@504>
2019-12-23 20:40:30 +0530

    Programs

---
+++ 23-12-2019(EntityCodeFirst)/Models/NorthwindContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;//provides DBcontext,database,DBset
+
+namespace EntityCodeFirst.Models
+{
+    //accessing Dbcontext,passing name of connection string,which we want to connect
+    public class NorthwindContext:DbContext
+    {
+        public NorthwindContext()
+            :base("name=NorthwindConnection")
+        {
+
+        }
+        public DbSet<Category> CategoriesTable { get; set; }
+        public DbSet<Product> ProductTable { get; set; }
+    }
+}
+//when we created NorthwindContext of this,that is responsible for communicate
+//with Connection string
---
+++ 23-12-2019(EntityCodeFirst)/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace EntityCodeFirst.Models
+{
+    [Table("tblProduct")]
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        //primary key that define in category,that is used in F.K in product
+        public Category Category { get; set; }
+    }
+}
