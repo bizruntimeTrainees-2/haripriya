86ffd37199b9dc406f9103ffefcf47e5dae3b743
haripriya@504 <anapalaharipriya1998@gmail.com>
2019-12-24 20:35:16 +0530

    Program

---
+++ 24-12-2019(EfDb)/EntityCodeFirst/EntityCodeFirst/Controllers/NorthwindController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EntityCodeFirst.Models;//through that we are going to access models
+
+namespace EntityCodeFirst.Controllers
+{
+    public class NorthwindController : Controller
+    {
+        NorthwindContext db = new NorthwindContext();
+        //db is responsible for comm with DB connection string(NorthwindContext)
+        public ViewResult Index()
+        {
+           // db.CategoriesTable.ToList();
+            //return list of categories
+            db.ProductTable.ToList();
+            return View(db.CategoriesTable.ToList());
+        }
+    }
+}
+//Index action communicate with db and inter act with CategoriesTable,ProductTable
